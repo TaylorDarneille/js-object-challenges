dc28a6bf808d61fccac0032b2d8d75cd5108332c
ktalens <ktal66@yahoo.com>
2020-08-25 19:48:26 -0700

    fatima-krystle_talens-pizzas submission

---
+++ js/fatima-krystle_talens-pizzas.js
@@ -0,0 +1,27 @@
+var menu ={
+    cheese: {
+      toppings:["cheese","sauce","extra cheese"],
+      price: 4
+      },
+    pepperoni: {
+      toppings:["cheese","sauce","pepperoni"],
+      price: 5
+      },
+    margherita:{
+      toppings:["cheese","tomato","basil"],
+      price: 6
+      },
+    combo:{
+      toppings:["cheese","sausage","pepperoni"],
+      price: 6
+      },
+    hawaiian: {
+      toppings:["cheese","canadian bacon","pineapple"],
+      price: 4
+      }
+  }
+  
+  
+  console.log(Object.keys(menu))
+  
+  console.log(Object.hasOwnProperty("banana"))

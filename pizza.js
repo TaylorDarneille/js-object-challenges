77ac20f39024db95f0f2fd69fcc8b50969147ccb
taylorschmidt <taylorhillschmidt@gmail.com>
2020-08-25 19:55:59 -0600

    Taylor Schmidt Object Deliverable

---
+++ pizza.js
@@ -0,0 +1,30 @@
+// Taylor Schmidt - Objects Deliverable
+
+//#1
+var menu = {
+    bbq: {
+      toppings: ['cheese', 'chicken', 'onions'],
+      price: 10
+    },
+    pepperoni: {
+      toppings: ['cheese', 'pepperoni', 'garlic'],
+      price: 12
+    },
+    mediterranean: {
+      toppings: ['chicken', 'olives', 'onions'],
+      price: 15
+    },
+    supreme: {
+      toppings: ['sausage', 'olives', 'onions'],
+      price: 15,
+    },
+    meatLovers: {
+      toppings:['sausage', 'chicken', 'beef'],
+      price: 16
+    }
+  };
+  ///#2
+  console.log(Object.keys(menu));
+  //#3
+  console.log(menu.Banana);
+  

430a5ac66292fd84154fa03e557f9710d5474e9d
cam-em <cmoreno.1117@gmail.com>
2020-08-25 21:28:10 -0500

    added solution javascript file

---
+++ js/pizza-cameron-moreno-bruno-da-silva.js
@@ -0,0 +1,24 @@
+const menu = {
+  pepperoni: const pizza = {
+    toppings: ['cheese', 'pizza sauce', 'pepperoni'],
+    price: 10
+  },
+  pineapple: const pizza = {
+    toppings: ['cheese', 'pizza sauce', 'pineapple'],
+    price: 11
+  },
+  spinach: const pizza = {
+    toppings: ['cheese', 'pizza sauce', 'pepperoni'],
+    price: 3
+  },
+  anchovie: const pizza = {
+    toppings: ['cheese', 'pizza sauce', 'anchovie'],
+    price: 3;
+  },
+  buffalo: const pizza = {
+    toppings: ['cheese', 'pizza sauce', 'chicken'],
+    price: 3;
+  }
+}
+
+console.log(menu);
